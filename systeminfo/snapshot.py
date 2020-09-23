cd726188be9162dff4821c807d4a6ed0a19407f2
shnipa <nikitashnipov@gmail.com>
2020-09-23 22:51:27 +0300

    update

--- systeminfo/snapshot.py
+++ systeminfo/snapshot.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import time
 from datetime import datetime
+
 number = 0
 
 parser = argparse.ArgumentParser(description='parser of arguments')
@@ -10,23 +11,29 @@ parser.add_argument("-t", help="Interval between snapshots", type=int, default=3
 parser.add_argument("-d", help="Output file type txt or json", default="txt")
 args = parser.parse_args()
 
+
+def get_cpu():
+    return psutil.cpu_percent()
+
+
+def get_mem():
+    return psutil.disk_usage("/").percent
+
+
+def get_vmem():
+    return psutil.virtual_memory().percent
+
+
 class Monitor:
     def __init__(self):
-        self.cpu = self.get_cpu()
-        self.mem = self.get_mem()
-        self.vmem = self.get_vmem()
+        self.cpu = get_cpu()
+        self.mem = get_mem()
+        self.vmem = get_vmem()
 
-    def get_cpu(self):
-        return psutil.cpu_percent()
-
-    def get_mem(self):
-        return psutil.disk_usage("/").percent
-
-    def get_vmem(self):
-        return psutil.virtual_memory().percent
 
 def ctime():
     return str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
 
 def output():
     if args.d == "json":
@@ -36,8 +43,8 @@ def output():
             "CPU load": Monitor().cpu,
             "memory": Monitor().mem,
             "virtual memory": Monitor().vmem
-            }
-        f = open("monitor.json", "a") 
+        }
+        f = open("monitor.json", "a")
         json.dump(data, f, indent=3)
         f.write("\n")
         f.close()
@@ -45,19 +52,20 @@ def output():
         return None
     else:
         n = number
-        timestamp=ctime(),
-        CPU_load=Monitor().cpu,
-        memory=Monitor().mem,
-        virtual_memory=Monitor().vmem
+        timestamp = ctime(),
+        CPU_load = Monitor().cpu,
+        memory = Monitor().mem,
+        virtual_memory = Monitor().vmem
         f = open("monitor.txt", "a")
-        f.write("snapshot %s timestamp %s CPU load %s memory %s virtual_memory %s\n" % (n, timestamp, CPU_load, memory, virtual_memory))
+        f.write("snapshot %s timestamp %s CPU load %s memory %s virtual_memory %s\n" % (
+            n, timestamp, CPU_load, memory, virtual_memory))
         f.close()
         print("monitor.txt is creating ...")
         return None
+
 
 while True:
     number += 1
     output()
     time.sleep(args.t)
-main()
 
