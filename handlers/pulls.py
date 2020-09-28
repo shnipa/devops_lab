98ba312d2381dde19edc7e047c9ddcac400830e9
shnipa <nikitashnipov@gmail.com>
2020-09-28 14:48:22 +0300

    update

--- handlers/pulls.py
+++ handlers/pulls.py
@@ -2,8 +2,8 @@ import requests
 
 
 def get_git_json():
-    NAME = "........."
-    PASS = "........."
+    NAME = "*******"
+    PASS = "*******"
     API = "https://api.github.com/repos/alenaPy/devops_lab/pulls?per_page=100&state=all"
     request = requests.get(API, auth=(NAME, PASS))
     key = request.json()
@@ -15,13 +15,13 @@ def get_array_object(i):
 
 
 def get_pulls(state):
-    test_res = get_git_json()
+    res = get_git_json()
     if state == "open" or state == "closed":
-        return get_data_res(test_res, state)
+        return get_data_res(res, state)
     elif state == "accepted" or state == "needs work":
-        return get_data_label(test_res, state)
+        return get_data_label(res, state)
     else:
-        return get_data_all(test_res, state)
+        return get_data_all(res)
 
 
 def get_data_res(res, state):
@@ -43,8 +43,9 @@ def get_data_label(res, state):
     return arr
 
 
-def get_data_all(res, state):
+def get_data_all(res):
     arr = []
     for i in res:
         arr.append(get_array_object(i))
     return arr
+
---
+++ tests/test_shnipov.py
@@ -0,0 +1,37 @@
+json = [
+    {
+        "html_url": "https://github.com/alenaPy/devops_lab/pull/1",
+        "number": "1",
+        "state": "open",
+        "title": "Homework1",
+        "labels": [
+            {
+                "name": "accepted"
+            }
+        ]
+    },
+    {
+        "html_url": "https://github.com/alenaPy/devops_lab/pull/2",
+        "number": "2",
+        "state": "closed",
+        "title": "Homework2",
+        "labels": [
+            {
+                "name": "needs work"
+            }
+        ]
+    }
+]
+
+result_data_all = [
+    {
+        "num": "1",
+        "title": "Homework1",
+        "link": "https://github.com/alenaPy/devops_lab/pull/1",
+    },
+    {
+        "num": "2",
+        "title": "Homework2",
+        "link": "https://github.com/alenaPy/devops_lab/pull/2",
+    }
+]
